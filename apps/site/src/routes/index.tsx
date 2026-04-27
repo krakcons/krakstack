4afd917452d161c4f0f9955edd07e7ea2cdf9f61
Billy Hawkes <billyhawkes02@gmail.com>
2026-04-27 11:38:16 -0400

    task: clean old registry files, add data-table messages

--- apps/site/src/routes/index.tsx
+++ apps/site/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { m } from '@/paraglide/messages'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: Home })
@@ -7,7 +8,7 @@ function Home() {
     <div className="p-8">
       <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
       <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
+        {m.home_page()}
       </p>
     </div>
   )
